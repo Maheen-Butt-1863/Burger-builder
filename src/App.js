002e0b1ab7050c12528ff3cf38201946d0b89844
Maheen Butt <maheen.butt@devsinc.com>
2024-01-18 16:18:18 +0500

    CREATE: implemented the increment decrement logic of ingredients

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/navbar';
+import Ingredients from './components/ingredients';
 
 function App() {
   return (
     <>
       <Navbar title='Burger Builder'/>
+      <Ingredients/>
     </>
   );
 }
--- src/components/ingredients.js
+++ src/components/ingredients.js
@@ -1,6 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Ingredients(){
-    return(
+export default function Ingredients() { 
+    const [ingredientCounts, setIngredientCounts] = useState({
+    lettuce: 0,
+    bacon: 0,
+    cheese: 0,
+    meat: 0
+  });
 
-    )}
+  const ingredients = ["lettuce", "bacon", "cheese", "meat"];
+
+  const addRemoveIngredient = (action, ingredient) => {
+    setIngredientCounts(prevCounts => {
+      const updatedCounts = { ...prevCounts };
+      console.log("updated",updatedCounts)
+      let count = updatedCounts[ingredient];
+
+      if (action === "add") {
+        count = count + 1;
+      } else {
+        count = Math.max(count - 1, 0);
+      }
+
+      updatedCounts[ingredient] = count;
+    return updatedCounts;
+    });
+  };
+
+    
+  
+    return <>
+             <div className="burgerIngredients">
+                <div className="topSide"></div>
+                <div className="bottomSide"></div>
+            </div>
+            <div className="centered-container">
+                <div className="ingredients-container">
+                    <p>Current price: <strong>$3.50</strong></p>
+                    {ingredients.map((ingredient, index) => (
+                        <div key={index} className="ingredient-row">
+                        <p><strong>{ingredient}</strong></p>
+                        <button onClick={() => addRemoveIngredient('add', ingredient)} className="action-button">More</button>
+                        <button onClick={() => addRemoveIngredient('remove', ingredient)} className="action-button">Less</button>
+                        </div>
+                    ))}
+                    <button className="order-button">ORDER NOW</button>
+                </div>
+            </div>
+    </>
+        
+  }
